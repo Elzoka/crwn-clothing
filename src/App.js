0e0c9b749f4d1da57893076f37d8d136bc0a5a86
elzoka <mahmoud_elzoka@yahoo.com>
2019-10-26 22:53:44 +0200

    add the sign in form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
