23bfb39f34b9aa35dd0aa2059cf334647fbe9e44
elzoka <mahmoud_elzoka@yahoo.com>
2019-10-31 14:45:56 +0200

    add redux to the shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import {selectCollections} from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+const ShopPage = ({collections}) => (
+    console.log(collections) ||
+    <div className="shop-page">
+        {collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps}/>
+        ))}
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
-            </div>
-        )
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
